a4beefbb3b5f26d2fdf58f76732bf143ca26d74a
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-01-09 14:18:32 +0300

    chore(personal-blog) :: update dummy image

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,12 @@ import * as S from "./styles";
 const Card = () => {
   return (
     <S.Card>
-      <img src="https://picsum.photos/180/180" alt="" width="48" height="48" />
+      <img
+        src="https://doodleipsum.com/180x180/avatar-3?bg=825DEB&i=cb2d7620cdcb7f2847d8c49486bb0343"
+        alt=""
+        width="48"
+        height="48"
+      />
       <div className="card-information">
         <h1>John Doe</h1>
         <small>Software Developer | @Company</small>
